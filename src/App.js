94f924e0dfa5d0081a341f64b6d9d66c2e784381
Kuroo-nekoo <phamtrihung5363@gmail.com>
2022-06-27 09:57:48 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import { Route, Routes } from "react-router";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Navbar from "./components/Navbar";
+import Register from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
       <Routes>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
