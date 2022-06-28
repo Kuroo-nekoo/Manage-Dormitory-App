7dd1e6192e5a1a29f9c8703d948ab7607e1706a5
Kuroo-nekoo <phamtrihung5363@gmail.com>
2022-06-28 18:11:59 +0700

    chore

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
   return (
     <div className="border border-solid border-gray-600 flex justify-around fixed w-full items-center">
       <div className="py-2 px-3 inline-block">
@@ -12,7 +11,6 @@ const Navbar = () => {
         <div className="py-2 px-3">Account</div>
         <div className="space-x-4 hidden group-hover:block absolute rounded border-solid border-black border z-10 bg-white">
           <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
         </div>
       </div>
     </div>
