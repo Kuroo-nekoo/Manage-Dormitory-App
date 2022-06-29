b7b1f32c0ef968e0839a7af965ac8e50d5965c7f
Krotohmaki <tri.pm.1405@gmail.com>
2022-06-29 07:57:57 +0700

    hien thi tam thoi lich su dang ki phong

--- src/pages/StdRoomRegistered/StdRoomRegistered.jsx
+++ src/pages/StdRoomRegistered/StdRoomRegistered.jsx
@@ -17,9 +17,18 @@ const StdRoomRegistered = () => {
   }, []);
 
   if ( Object.keys(room).length ) {
+    console.log(room.data.data);
+
     return (
-      <div className="w-full h-screen flex justify-center items-center">
-        {JSON.stringify(room)}
+      <div className="w-full h-screen justify-center items-center">
+        <h1>Thông tin đăng ký kí túc xá</h1>
+        <div>{room.data.data.student_id}</div>
+        <div>{room.data.data.name}</div>
+        <div>{room.data.data.register_time}</div>
+        <div>{room.data.data.registration_status}</div>
+
+        <button className="border-solid border-2 border-sky-500">Hủy đăng ký</button>
+        <button className="border-solid border-2 border-sky-500">Sửa đơn đăng ký</button>
       </div>
     )
   }
